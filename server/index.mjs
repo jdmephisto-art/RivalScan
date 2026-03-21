8cfbc9e79496edb876dd212f56b187b00b39e026
Jack Dzmitrevich <jdmephisto@gmail.com>
2026-03-21 22:54:52 +0300

    Fix index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '../dist')));
   
   app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(join(__dirname, '../dist/index.html'));
   });
 }
 
